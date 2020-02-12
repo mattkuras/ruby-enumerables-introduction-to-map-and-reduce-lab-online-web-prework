a5edde5bdcb8a39e17e021ba61becb1ac0efb5e7
Matt Kuras <mattkuras@gmail.com>
2020-02-12 19:55:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,16 @@ def map_to_square(array)
     end
   new_array
 end 
+
+def reduce_to_total(array, starting_point=0)
+  total = starting_point
+  counter = 0 
+  while counter < array.length do 
+    total = starting_point + array[counter]
+    counter += 1 
+    end
+  total 
+end
+
+
+
