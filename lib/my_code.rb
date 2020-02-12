a57d1a6bf2fbfd1d17c3c490cd525dbbada012be
Matt Kuras <mattkuras@gmail.com>
2020-02-12 19:44:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(array)
   new_array = []
   counter = 0 
   while counter < array.length do 
-    new_array << array[counter] 
+    new_array << array[counter] ** 2 
     counter += 1 
     end
   new_array
