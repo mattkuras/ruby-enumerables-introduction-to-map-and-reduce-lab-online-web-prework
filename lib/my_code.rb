efa34ce5e55ac9c1f8c350cec6502b9ade6fc4ff
Matt Kuras <mattkuras@gmail.com>
2020-02-12 19:42:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map_to_negativize(array)
   new_array
 end 
 
+def map_to_no_change(array)
+  new_array = []
+  counter = 0 
+  while counter < array.length do 
+    new_array << array[counter] 
+    counter += 1 
+    end
+  new_array
+end 
